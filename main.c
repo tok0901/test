7ec1ed1927988cb91000dd850267b94b1ee7774a
tok0901 <pingyerenxi@gmail.com>
2024-06-19 11:15:19 +0900

    Update main.c

--- main.c
+++ main.c
@@ -7,7 +7,23 @@
 #include "Queue.h"
 #include "Command.h"
 #include "Fault_dic.h"
-#include "module.h"
+#include "FFR.h"
+
+//プロトタイプ宣言////////////////////////////////////////////////////////////////
+
+//ネットリスト読込み
+int command(int argc, char** argv);
+int input_f(char* tp, char* pin, char* v);
+int make_net(NLIST* nl);
+
+
+//故障シミュレーション
+int devide_ffr(NLIST** sort_net);
+int losic_simulation(int test_number, int sim_test, NLIST** sort_net);
+int SAF_PPSFP(int test_number, int sim_test, FFR* ffr);
+//int make_dic(int test_number,int sim_test,NLIST** sort_net);
+
+//////////////////////////////////////////////////////////////////////////////////
 
 
 int main(int argc, char* argv[]) {
@@ -32,8 +48,8 @@ int main(int argc, char* argv[]) {
 		return 0;
 	}
 
-	int test_number, sim_test = 64, max_test;
-	for (test_number = 0; test_number < n_test;test_number+=sim_test) {
+	int sim_test = 64, max_test;
+	for (int test_number = 0; test_number < n_test;test_number+=sim_test) {
 
 		max_test = n_test - test_number;
 
@@ -46,11 +62,26 @@ int main(int argc, char* argv[]) {
 			return 0;
 		}
 
-		if (SAF_PPSFP(test_number,sim_test) != 1) {
+		if (SAF_PPSFP(test_number,sim_test,ffr) != 1) {
 			printf("\n\nPPSFPエラー\n");
 			return 0;
 		}
 
+		/*if (make_dic(test_number, sim_test, sort_net) != 1) {
+			printf("\n\n故障辞書生成エラー\n");
+			return 0;
+		}*/
+
+		printf("%d番目から%d番目のテストパターンによる故障検出完了\n", test_number, test_number + sim_test);
+
+	}
+
+	for (int test_number = 0; test_number < n_test; test_number++) {
+		printf("tp%d\t検出故障数:%d\n", test_number,dic[test_number].n_fault);
+
+		for (int net_number = 0; net_number < dic[test_number].n_fault; net_number++) {
+			printf("s-a-%d\t%s\n", dic[test_number].saf_flag[net_number], dic[test_number].fault[net_number]->name);
+		}
 	}
 
 	return 0;
