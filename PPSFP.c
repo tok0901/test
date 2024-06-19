5a4a9eceb48aa55cd1d361766a67dd8dd1b8eba9
tok0901 <pingyerenxi@gmail.com>
2024-06-19 11:14:01 +0900

    Update PPSFP.c

--- PPSFP.c
+++ PPSFP.c
@@ -7,28 +7,58 @@
 #include "Queue.h"
 #include "Command.h"
 #include "Fault_dic.h"
-#include "module.h"
+#include "FFR.h"
+
+//プロトタイプ宣言
+//クリティカル経路導出関数
+int SAF_CPT1(int test_number, int tst_number, NLIST* sim_net);
+int SAF_CPT0(int test_number, int tst_number, NLIST* sim_net);
 
 
-int SAF_PPSFP(int test_number,int sim_test) {
 
-	int ffr_number;
+int SAF_PPSFP(int test_number,int sim_test, FFR* ffr) {
+
+	int ffr_number, tst_number, cpt_flag;
 		
 	//CPT実行
-	for (ffr_number = n_ffr; ffr_number >= 0; ffr_number--) {						//外部入力側のFFRからCPT実行
+	for (ffr_number = 0; ffr_number <n_ffr; ffr_number++) {								//外部出力側のFFRからCPT実行
 
-		if (ffr[ffr_number].fos->value == 1) {										//対象信号線の正常値が1の時
+		for (tst_number = 0; tst_number < sim_test; tst_number++) {						//パターン並列(64bit)
 
-			if (SAF_CPT1(ffr[ffr_number].fos, test_number,sim_test) != 1) {			//SAF_CPT1呼出し
-				return 0;
+			cpt_flag = 0;
+			if (ffr[ffr_number].fos->n_out >= 2) {										//対象信号線がFOSである時
+
+				for (int i = 0; i < ffr[ffr_number].fos->n_out; i++) {					//FoutBlanchのdetectability判定
+					if (ffr[ffr_number].fos->out[i]->detec[tst_number] == 1) {						//fosの出力線がdetectability=1であるかどうか(trueならbreak→CPT実行)
+						cpt_flag = 1;
+						break;
+					}
+				}
 			}
 
-		}
+			else {																		//対象信号線が外部出力線である時
+				cpt_flag = 1;
+			}
 
-		else if (ffr[ffr_number].fos->value == 0) {									//対象信号線の正常値が0の時
+			//CPT実行
+			if (cpt_flag == 1) {
 
-			if (SAF_CPT0(ffr[ffr_number].fos, test_number, sim_test) != 1) {		//SAF_CPT0呼出し
-				return 0;
+				if (ffr[ffr_number].fos->value[tst_number] == 1) {						//対象信号線の正常値が1の時
+
+					if (SAF_CPT1(test_number, tst_number, ffr[ffr_number].fos) != 1) {	//SAF_CPT1呼出し
+						return 0;
+					}
+
+				}
+
+				else if (ffr[ffr_number].fos->value[tst_number] == 0) {					//対象信号線の正常値が0の時
+
+					if (SAF_CPT0(test_number, tst_number, ffr[ffr_number].fos) != 1) {	//SAF_CPT0呼出し
+						return 0;
+					}
+
+				}
+
 			}
 
 		}
@@ -37,7 +67,7 @@ int SAF_PPSFP(int test_number,int sim_test) {
 
 	}
 
-	printf("%d番目から%d番目のテストパターンによるPPSFP完了!\n\n", test_number, test_number + sim_test);
+	printf("\nPPSFP完了!\n\n");
 
 	return 1;
 }
