de6b57f6cb35d0a226a0c7f06eac9c4c967e2a8a
tok0901 <pingyerenxi@gmail.com>
2024-06-30 03:31:53 +0900

    Update Pulse_Output_Value.c

--- Pulse_Output_Value.c
+++ Pulse_Output_Value.c
@@ -13,18 +13,36 @@
 char* Pulse_Output_Value(int tst_number, NLIST** sort_net) {
 
 	int net_number;
+	char* po_temp;
+	po_temp = (char*)malloc(sizeof(char) * n_po);
 
-	for (net_number = 0; net_number < n_pi; net_number++) {
+	for (net_number = 0; net_number < n_po; net_number++) {
 		
+		//外部出力線に故障が伝搬する時(故障値の出力応答値を格納)
 		if (po[net_number]->value_fault_flag[tst_number] == 1) {
 
 			if (po[net_number]->value[tst_number] == 0) {
+				po_temp[net_number] = "1";
+			}
+			else if (po[net_number]->value[tst_number] == 1) {
+				po_temp[net_number] = "0";
+			}
+		}
 
+		//外部出力線に故障が伝搬しない時(正常値の出力応答値を格納)
+		else if (po[net_number]->value_fault_flag[tst_number] == 0) {
+
+			if (po[net_number]->value[tst_number] == 0) {
+				po_temp[net_number] = "0";
+			}
+			else if (po[net_number]->value[tst_number] == 1) {
+				po_temp[net_number] = "1";
 			}
 		}
 
 
 	}
 
+	return po_temp;
 
 }
