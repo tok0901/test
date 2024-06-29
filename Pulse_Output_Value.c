e6a3213dee25440fa1ed679021a70c166cbcc2ed
tok0901 <pingyerenxi@gmail.com>
2024-06-30 03:19:02 +0900

    Create Pulse_Output_Value.c

---
+++ Pulse_Output_Value.c
@@ -0,0 +1,21 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "Netlist.h"
+#include "Queue.h"
+#include "Command.h"
+#include "Fault_dic.h"
+#include "FFR.h"
+
+int Pulse_Output_Value(int tst_number, NLIST** sort_net) {
+
+	int net_number;
+
+	for (net_number = 0; net_number < n_pi; net_number++) {
+
+	}
+
+
+}
