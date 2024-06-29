8227b12549b05fbea620fec4d0c95d2c08f23083
tok0901 <pingyerenxi@gmail.com>
2024-06-30 03:25:15 +0900

    Update Pulse_Output_Value.c

--- Pulse_Output_Value.c
+++ Pulse_Output_Value.c
@@ -9,11 +9,20 @@
 #include "Fault_dic.h"
 #include "FFR.h"
 
-int Pulse_Output_Value(int tst_number, NLIST** sort_net) {
+//出力応答値算出関数
+char* Pulse_Output_Value(int tst_number, NLIST** sort_net) {
 
 	int net_number;
 
 	for (net_number = 0; net_number < n_pi; net_number++) {
+		
+		if (po[net_number]->value_fault_flag[tst_number] == 1) {
+
+			if (po[net_number]->value[tst_number] == 0) {
+
+			}
+		}
+
 
 	}
 
