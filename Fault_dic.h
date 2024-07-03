597d1af461ddf872295c7577e4c4506760289e99
tok0901 <pingyerenxi@gmail.com>
2024-07-04 05:08:10 +0900

    Update Fault_dic.h

--- Fault_dic.h
+++ Fault_dic.h
@@ -1,9 +1,6 @@
 //故障辞書構造体定義
 typedef struct Fault_dic {
 	int tp;						//テストパターン番号
-	NLIST** fault;				//検出故障
-	int* saf_flag;				//検出故障フラグ(0縮退故障or1縮退故障)
-	int n_fault;				//検出故障数
 	NLIST*** unconf_fault;		//未識別故障集合
 	int** unconf_saf_flag;		//検出未識別故障フラグ(0縮退故障or1縮退故障)
 	char** po_value;			//ハッシュ表(外部出力値or剰余値)
@@ -24,11 +21,14 @@ typedef struct Unconf_Fault_Pair {
 	int n_grp;					//未識別故障集合数
 	int insert_number;			//新規検出故障格納インデックス番号
 	int confirm_number;			//識別細分化故障格納インデックス番号
+	int** confirm_flag;			//識別細分化フラグ(1:識別細分化可能,other:識別細分化不能)
+	int* n_confirm_flag;			//識別細分化可能フラグ数(n_unconf_fault以上の時は細分化操作不能)
 
 
 }HASH;
 
 HASH hash;
 
+//make_hash_tbl内関数プロトタイプ宣言
 char* Pulse_Output_Value(int tst_number, NLIST** sort_net);
-int hash_insert_number(int test_number, int tst_number, char* temp_po);
+void hash_insert(int test_number, int tst_number, char* temp_po);
