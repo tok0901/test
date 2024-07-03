d57af0b9e17bd71900b101c25e01ecdd713d4a9d
tok0901 <pingyerenxi@gmail.com>
2024-07-04 08:51:04 +0900

    Update Fault_dic.h

--- Fault_dic.h
+++ Fault_dic.h
@@ -2,28 +2,28 @@
 typedef struct Fault_dic {
 	int tp;						//テストパターン番号
 	NLIST*** unconf_fault;		//未識別故障集合
-	int** unconf_saf_flag;		//検出未識別故障フラグ(0縮退故障or1縮退故障)
+	int* n_unconf_fault;		//未識別故障数
+	int** unconf_saf_flag;		//検出故障フラグ(0縮退故障or1縮退故障)
 	char** po_value;			//ハッシュ表(外部出力値or剰余値)
-	int* n_unconf_fault;		//未識別故障数(ハッシュ表内)
 	int n_grp;					//未識別故障集合数
-	int insert_number;			//検出故障格納インデックス番号
-	int** hash_number;			//未識別故障格納ハッシュ番号
+	int insert_number;			//検出故障格納要素番号
+	int** hash_number;			//未識別故障格納ハッシュ番号(対象故障-未識別故障ペア配列の要素番号)
 
 }DICT;
 
 DICT* dic;
 
 
-//未識別故障ペア格納ハッシュ配列構造体定義
+//未識別故障ペア配列構造体定義
 typedef struct Unconf_Fault_Pair {
 	int** saf_flag;				//検出故障フラグ
 	NLIST*** unconf_fault;		//未識別故障集合
-	int* n_unconf_fault;		//未識別故障ペア数
+	int* n_unconf_fault;		//未識別故障数
 	int n_grp;					//未識別故障集合数
-	int insert_number;			//新規検出故障格納インデックス番号
-	int confirm_number;			//識別細分化故障格納インデックス番号
-	int** confirm_flag;			//識別細分化フラグ(1:識別細分化可能,other:識別細分化不能)
-	int* n_confirm_flag;			//識別細分化可能フラグ数(n_unconf_fault以上の時は細分化操作不能)
+	int insert_number;			//新規未識別故障集合格納要素番号
+	int confirm_number;			//細分化未識別故障集合格納要素番号
+	int** confirm_flag;			//細分化フラグ(1:識別細分化可能,other:識別細分化不能)
+	int* n_confirm_flag;		//細分化可能フラグ数(細分化未識別故障数:n_unconf_fault以上の時は細分化操作不能)
 
 
 }HASH;
