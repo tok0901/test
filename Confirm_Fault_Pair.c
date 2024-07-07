44a05e49c0f129885bf4168a560d959048afe242
tok0901 <pingyerenxi@gmail.com>
2024-07-08 06:57:26 +0900

    Update Confirm_Fault_Pair.c

--- Confirm_Fault_Pair.c
+++ Confirm_Fault_Pair.c
@@ -18,7 +18,7 @@ int* Subdivide_Judge(HASH hash,DICT sim_dic,int hash_number);
 //未識別故障ペア取得関数(return 0:全検出故障の識別完了,return 1:正常終了→スコア計算)
 int Confirm_Fault_Pair(HASH hash, DICT* dic) {
 
-	int test_number, hash_number, fault_number, search_number,i;																	//ループ変数
+	int test_number, hash_number, fault_number, search_number,i,x;																	//ループ変数
 	int n_grp, n_unconf_fault,count,insert_number, confirm_number,devide_number,cur_number,saf_flag, flag; int* devide_hash_number;	//操作変数
 	NLIST* conf_fault;
 
@@ -41,6 +41,8 @@ int Confirm_Fault_Pair(HASH hash, DICT* dic) {
 
 				conf_fault = dic[test_number].unconf_fault[hash_number][fault_number];
 				saf_flag = dic[test_number].unconf_saf_flag[hash_number][fault_number];
+
+
 
 				//未識別故障ペア配列内に1つでも対象故障(conf_fault)が探索できたか
 				if (Confirm_Fault_Search(hash,dic[test_number],hash_number,fault_number) == 1) {
@@ -65,6 +67,7 @@ int Confirm_Fault_Pair(HASH hash, DICT* dic) {
 				//新規未識別故障ペア配列領域確保
 				hash.unconf_fault[insert_number] = (NLIST**)malloc(sizeof(NLIST*) * n_unconf_fault);
 				hash.n_unconf_fault[insert_number] = 0;
+				hash.n_index[insert_number] = n_unconf_fault;
 				hash.saf_flag[insert_number] = (int*)malloc(sizeof(int) * n_unconf_fault);
 				hash.confirm_flag[insert_number] = (int*)malloc(sizeof(int) * n_unconf_fault);
 				hash.n_confirm_flag[insert_number] = 0;
@@ -93,7 +96,8 @@ int Confirm_Fault_Pair(HASH hash, DICT* dic) {
 
 				//新規未識別故障ペア配列領域確保
 				hash.unconf_fault[insert_number] = (NLIST**)malloc(sizeof(NLIST*) * (n_unconf_fault-count));
-				hash.n_unconf_fault[insert_number] = n_unconf_fault - count;
+				hash.n_unconf_fault[insert_number] = 0;
+				hash.n_index[insert_number] = n_unconf_fault - count;
 				hash.saf_flag[insert_number] = (int*)malloc(sizeof(int) * (n_unconf_fault-count));
 				hash.confirm_flag[insert_number] = (int*)malloc(sizeof(int) * (n_unconf_fault-count));
 				hash.n_confirm_flag[insert_number] = 0;
@@ -109,6 +113,7 @@ int Confirm_Fault_Pair(HASH hash, DICT* dic) {
 						hash.unconf_fault[insert_number][cur_number] = dic[test_number].unconf_fault[hash_number][search_number];
 						hash.saf_flag[insert_number][cur_number] = dic[test_number].unconf_saf_flag[hash_number][search_number];
 						hash.confirm_flag[insert_number][cur_number] = -1;
+						hash.n_unconf_fault[insert_number]++;
 						cur_number++;
 					}
 				}
@@ -144,16 +149,17 @@ int Confirm_Fault_Pair(HASH hash, DICT* dic) {
 
 					//新規未識別故障ペア配列領域確保
 					hash.unconf_fault[confirm_number] = (NLIST**)malloc(sizeof(NLIST*) * count);
-					hash.n_unconf_fault[confirm_number] = count;
+					hash.n_unconf_fault[confirm_number] = 0;
 					hash.saf_flag[confirm_number] = (int*)malloc(sizeof(int) * count);
 					hash.confirm_flag[confirm_number] = (int*)malloc(sizeof(int) * count);
 					hash.n_confirm_flag[confirm_number] = 0;
+					hash.n_index[confirm_number] = count;
 
 					//操作変数初期化
 					cur_number = 0;
 
 					//細分化可能未識別故障集合挿入
-					for (search_number = 0; search_number < hash.n_unconf_fault[devide_number]; search_number++) {
+					for (search_number = 0; search_number < hash.n_index[devide_number]; search_number++) {
 
 						//対象故障が細分化可能かどうか
 						if (hash.confirm_flag[devide_number][search_number] == 1) {
@@ -164,6 +170,8 @@ int Confirm_Fault_Pair(HASH hash, DICT* dic) {
 							cur_number++;
 
 							hash.unconf_fault[devide_number][search_number] = NULL;
+							hash.saf_flag[devide_number][search_number] = -1;
+							hash.confirm_flag[devide_number][search_number] = -1;
 							hash.n_unconf_fault[devide_number]--;
 						}
 					}
@@ -173,6 +181,10 @@ int Confirm_Fault_Pair(HASH hash, DICT* dic) {
 					hash.confirm_number++;
 					hash.insert_number = hash.confirm_number;
 					i++;
+
+					//細分化可能フラグ数リセット(hash_numberインクリメント前に必ず!)
+					hash.n_confirm_flag[devide_number] = 0;
+
 				}
 
 				//**************************************************************************************************************************************************
@@ -203,25 +215,29 @@ int Confirm_Fault_Pair(HASH hash, DICT* dic) {
 
 					//新規未識別故障ペア配列領域確保
 					hash.unconf_fault[confirm_number] = (NLIST**)malloc(sizeof(NLIST*) * count);
-					hash.n_unconf_fault[confirm_number] = count;
+					hash.n_unconf_fault[confirm_number] = 0;
 					hash.saf_flag[confirm_number] = (int*)malloc(sizeof(int) * count);
 					hash.confirm_flag[confirm_number] = (int*)malloc(sizeof(int) * count);
 					hash.n_confirm_flag[confirm_number] = 0;
+					hash.n_index[confirm_number] = count;
 
 					//操作変数初期化
 					cur_number = 0;
 
 					//細分化可能未識別故障集合挿入
-					for (search_number = 0; search_number < hash.n_unconf_fault[devide_number]; search_number++) {
+					for (search_number = 0; search_number < hash.n_index[devide_number]; search_number++) {
 
 						//細分化フラグが有効な故障を挿入
 						if (hash.confirm_flag[devide_number][search_number] == 1) {
 							hash.unconf_fault[confirm_number][cur_number] = hash.unconf_fault[devide_number][search_number];
 							hash.saf_flag[confirm_number][cur_number] = hash.saf_flag[devide_number][search_number];
 							hash.confirm_flag[confirm_number][cur_number] = -1;
+							hash.n_unconf_fault[confirm_number]++;
 							cur_number++;
 
 							hash.unconf_fault[devide_number][search_number] = NULL;
+							hash.saf_flag[devide_number][search_number] = -1;
+							hash.confirm_flag[devide_number][search_number] = -1;
 							hash.n_unconf_fault[devide_number]--;
 						}
 					}
@@ -231,6 +247,9 @@ int Confirm_Fault_Pair(HASH hash, DICT* dic) {
 					hash.confirm_number++;
 					hash.insert_number = hash.confirm_number;
 					i++;
+
+					//細分化可能フラグ数リセット(hash_numberインクリメント前に必ず!)
+					hash.n_confirm_flag[devide_number] = 0;
 				}
 
 			}
@@ -239,16 +258,36 @@ int Confirm_Fault_Pair(HASH hash, DICT* dic) {
 		
 		}//故障辞書内-全未識別故障集合参照終了
 
+		printf("\n************tp%d**********************************\n", test_number);
+
+		for (int y = 0; y < hash.n_grp; y++) {
+
+			printf("\n***Unconf_Fault_Pair%d***\n", y);
+
+			for (int z = 0; z < hash.n_index[y]; z++) {
+
+				if (hash.unconf_fault[y][z] != NULL) {
+
+					if (hash.saf_flag[y][z] == 1) {
+						printf("s-a-1\t%s\n", hash.unconf_fault[y][z]->name);
+					}
+					else if (hash.saf_flag[y][z] == 0) {
+						printf("s-a-0\t%s\n", hash.unconf_fault[y][z]->name);
+					}
+
+				}
+
+			}
+		}
+
 	}//全故障辞書配列参照終了
 
 	conf_fault_module = hash.n_grp;
 	if (conf_fault_module == n_net * 2) {
 
-		printf("\n\n全故障の識別完了\n\n");
 		return 0;
 	}
 
-	printf("\n\n未識別故障ペア取得完了\n\n");
 	return 1;
 
 }
@@ -259,7 +298,7 @@ int Confirm_Fault_Pair(HASH hash, DICT* dic) {
 int Confirm_Fault_Search(HASH hash, DICT sim_dic, int hash_number, int fault_number) {
 
 	//ループ変数,操作変数初期化
-	int devide_number, search_number, n_grp, n_unconf_fault;
+	int devide_number, search_number, n_grp, n_search;
 	n_grp = hash.n_grp;
 
 	//探索対象故障の定義
@@ -271,10 +310,10 @@ int Confirm_Fault_Search(HASH hash, DICT sim_dic, int hash_number, int fault_num
 	for (devide_number = 0; devide_number < n_grp; devide_number++) {
 
 		//ループ変数初期化
-		n_unconf_fault = hash.n_unconf_fault[devide_number];
+		n_search = hash.n_index[devide_number];
 
 		//未識別故障ペア内-全故障を参照
-		for (search_number = 0; search_number < n_unconf_fault; search_number++) {
+		for (search_number = 0; search_number < n_search; search_number++) {
 
 			//故障探索成功時
 			if ((hash.unconf_fault[devide_number][search_number] == sim_net) && (hash.saf_flag[devide_number][search_number] == sim_saf_flag)) {
@@ -297,13 +336,15 @@ int Confirm_Fault_Search(HASH hash, DICT sim_dic, int hash_number, int fault_num
 
 //未識別故障ペア細分化判定関数(細分化不能:return NULL,細分化可能:return int* devide_hash_number)
 //細分化不能ハッシュ番号は対象(delete_hash_number)の未識別故障ペアの細分化フラグ,細分化可能フラグ数を初期化すること。必ず！
-int* Subdivide_Judge(HASH hash,DICT sim_dic,int hash_number) {
+int* Subdivide_Judge(HASH hash, DICT sim_dic, int hash_number) {
 
 	int* devide_hash_number;			//細部化可能ハッシュ番号
 	int n_devide = 0;					//細分化可能ハッシュ数
 	int* delete_hash_number;			//リセットハッシュ番号
 	int n_delete = 0;					//リセットハッシュ数
 	int n_unconf_fault = sim_dic.n_unconf_fault[hash_number];
+	devide_hash_number = (int*)malloc(sizeof(int) * n_unconf_fault);
+	delete_hash_number = (int*)malloc(sizeof(int) * n_unconf_fault);
 
 	int fault_number, search_number, devide_number, flag, i;	//ループ変数
 	
@@ -311,6 +352,11 @@ int* Subdivide_Judge(HASH hash,DICT sim_dic,int hash_number) {
 
 		devide_number = sim_dic.hash_number[hash_number][fault_number];
 		flag = 1;
+
+		//参照ハッシュ番号が存在しない時(新規検出故障であるため細分化不能)
+		if (devide_number == -1) {
+			flag = 0;
+		}
 
 		for (i = 0; i < n_devide; i++) {
 			//既存のハッシュ番号かどうか
@@ -320,42 +366,47 @@ int* Subdivide_Judge(HASH hash,DICT sim_dic,int hash_number) {
 			}
 		}
 
-		//既存ハッシュ番号の時
-		if (flag == 0) {
-			break;
+		for (i = 0; i < n_delete; i++) {
+			if (delete_hash_number[i] == devide_number) {
+				flag = 0;
+				break;
+			}
 		}
 
-		//細分化可能フラグ数が未識別故障数より少ない時(細分化可能な時)
-		if (hash.n_confirm_flag[devide_number] < hash.n_unconf_fault[devide_number]) {
-			flag = 1;
-		}
-		//細分化可能フラグ数が未識別故障数に達している時(細分化不能な時→全フラグをリセット)
-		else {
-			flag = 0;
-		}
-		
-		//細分化可能ハッシュ番号生成
-		if (flag == 1) {
-			i = n_devide;
-			n_devide++;
-			devide_hash_number = (int*)realloc(devide_hash_number, sizeof(int) * n_devide);
-			devide_hash_number[i] = devide_number;
-		}
-		//細分化不能ハッシュ番号生成,細分化フラグのリセット
-		else if (flag == 0) {
-			i = n_delete;
-			n_delete++;
-			delete_hash_number = (int*)realloc(delete_hash_number, sizeof(int) * n_delete);
-			delete_hash_number[i] = devide_number;
+		//新規ハッシュ番号の時
+		if (flag != 0) {
 
-			//細分化フラグのリセット
-			int n_search = hash.n_unconf_fault[devide_number];
-			for (search_number = 0; search_number < n_search; search_number++) {
-
-				hash.confirm_flag[devide_number][search_number] = -1;
+			//細分化可能フラグ数が未識別故障数より少ない時(細分化可能な時)
+			if (hash.n_confirm_flag[devide_number] < hash.n_unconf_fault[devide_number]) {
+				flag = 1;
+			}
+			//細分化可能フラグ数が未識別故障数に達している時(細分化不能な時→全フラグをリセット)
+			else {
+				flag = 0;
 			}
 
-			hash.n_confirm_flag[devide_number] = 0;
+			//細分化可能ハッシュ番号生成
+			if (flag == 1) {
+				i = n_devide;
+				n_devide++;
+				devide_hash_number[i] = devide_number;
+			}
+			//細分化不能ハッシュ番号生成,細分化フラグのリセット
+			else if (flag == 0) {
+				i = n_delete;
+				n_delete++;
+				delete_hash_number[i] = devide_number;
+
+				//細分化フラグのリセット
+				int n_search = hash.n_index[devide_number];
+				for (search_number = 0; search_number < n_search; search_number++) {
+
+					hash.confirm_flag[devide_number][search_number] = -1;
+				}
+
+				hash.n_confirm_flag[devide_number] = 0;
+
+			}
 
 		}
 
@@ -369,7 +420,6 @@ int* Subdivide_Judge(HASH hash,DICT sim_dic,int hash_number) {
 	else if (n_devide > 0) {
 		i = n_devide;
 		n_devide++;
-		devide_hash_number = (int*)realloc(devide_hash_number, sizeof(int) * n_devide);
 		devide_hash_number[i] = -1;
 		return devide_hash_number;
 	}
