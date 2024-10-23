91b4e4a693d8f766332480d3c43d74517eb9e8ff
tok0901 <pingyerenxi@gmail.com>
2024-10-23 09:05:36 +0900

    Update FFR.h

--- FFR.h
+++ FFR.h
@@ -1,16 +1,20 @@
 //FFR構造体定義
 typedef struct Fanout_FreeRegion {
 
-	int						ffr_id;				//FFRのID
-	int						n_out;				//他FFRへの出力数
-	struct FoutFreeRegion** out;				//他出力FFRへのポインタ
-	int						n_in;				//他FFRへの入力数
-	struct FoutFreeRegion** in;					//他入力FFRへのポインタ
-	int						n_pi;				//FFR内の外部入力数
-	int						n_in_net;			//FFR内の入力数(入力はINとFOUTブランチの2種類　※他FFRからの入力では無い)
-	int						po_flag;			//外部出力格納フラグ(1:FFR内に外部出力線を持つ,0:持たない)
-	NLIST* fos;									//FOUT_STEMへのポインタ
+	int							ffr_id;				//FFRのID
+	int							n_out;				//他FFRへの出力数
+	struct Fanout_FreeRegion**	out;				//他出力FFRへのポインタ
+	int							n_in;				//他FFRへの入力数
+	struct Fanout_FreeRegion**	in;					//他入力FFRへのポインタ
+	int							n_pi;				//FFR内の外部入力数
+	int							n_in_net;			//FFR内の入力数(入力はINとFOBの2種類　※他FFRからの入力では無い)
+	int							po_flag;			//外部出力格納フラグ(1:FFR内に外部出力線を持つ,0:持たない)
+	NLIST*						fos;				//FOUT_STEMへのポインタ
+	unsigned long long int*		po_fault_flag;		//故障伝搬フラグ(po,tpiの故障伝搬フラグを格納)
+	int							tpi_flag;			//FFR内観測ポイント数
 
 } FFR;
 
 FFR* ffr;
+
+void ffr_reset(void);
