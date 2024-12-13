e692cc4833c1f950161c146824537227d1683e78
tok0901 <pingyerenxi@gmail.com>
2024-12-13 19:45:11 +0900

    Update Fault_dic.h

--- Fault_dic.h
+++ Fault_dic.h
@@ -3,8 +3,8 @@ typedef struct Fault_dic {
 	int tp;						//テストパターン番号
 	NLIST*** unconf_fault;		//未識別故障集合
 	int* n_unconf_fault;		//未識別故障数
-	short** unconf_saf_flag;		//検出故障フラグ(0縮退故障or1縮退故障)
-	char** po_value;			//ハッシュ表(外部出力値or剰余値)
+	ULLI** unconf_saf_fault;	//縮退故障値(0縮退故障:0, 1縮退故障:1)
+	ULLI** po_val;				//ハッシュ表(出力応答値64bit×64テストパターン×剰余分)
 	int n_grp;					//未識別故障集合数
 	int insert_number;			//検出故障格納要素番号
 	int** hash_number;			//未識別故障格納ハッシュ番号(対象故障-未識別故障ペア配列の要素番号)
@@ -13,28 +13,12 @@ typedef struct Fault_dic {
 
 DICT* dic;
 
-
-//未識別故障ペア配列構造体定義
-typedef struct Unconf_Fault_Pair {
-	short** saf_flag;				//検出故障フラグ
-	NLIST*** unconf_fault;		//未識別故障集合
-	int* n_unconf_fault;		//未識別故障数
-	int* n_index;				//最大要素数
-	int n_grp;					//未識別故障集合数
-	int insert_number;			//新規未識別故障集合格納要素番号
-	int confirm_number;			//細分化未識別故障集合格納要素番号
-	short** confirm_flag;			//細分化フラグ(1:識別細分化可能,other:識別細分化不能)
-	short* n_confirm_flag;		//細分化可能フラグ数(細分化未識別故障数:n_unconf_fault以上の時は細分化操作不能)
-
-
-}HASH;
-
-HASH hash;
-
-//make_hash_tbl内関数プロトタイプ宣言
-char* Pulse_Output_Value(int tst_number, NLIST** sort_net);
-void hash_insert(int test_number, int tst_number, char* temp_po);
-void hash_reset(int test_dic);
+//make_DICT
+void malloc_DICT(int test_number, int sim_test);
+ULLI* calc_po_val(int test_dic,NLIST* sim_net,int tpi_flag);
+ULLI* observate_po_val(int test_dic,int flag);
+void hash_insert_DICT(int test_dic, ULLI* temp);
+void fault_detect_DICT(int test_dic, NLIST* sim_net);
 
 //外部変数
 
