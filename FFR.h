b4f77bc1c93db8f801951c588769249cbe266f39
tok0901 <pingyerenxi@gmail.com>
2024-12-13 19:45:30 +0900

    Update FFR.h

--- FFR.h
+++ FFR.h
@@ -10,8 +10,10 @@ typedef struct Fanout_FreeRegion {
 	int							n_in_net;			//FFR内の入力数(入力はINとFOBの2種類　※他FFRからの入力では無い)
 	int							po_flag;			//外部出力格納フラグ(1:FFR内に外部出力線を持つ,0:持たない)
 	NLIST*						fos;				//FOUT_STEMへのポインタ
-	unsigned long long int*		po_fault_flag;		//故障伝搬フラグ(po,tpiの故障伝搬フラグを格納)
-	int							tpi_flag;			//FFR内観測ポイント数
+	unsigned long long int*		po_fault_flag;		//出力応答値(po,tpiの故障値を格納)
+	int							n_unconf_fault_det;	//故障検出回数
+	int							n_unconf_fault;		//未識別故障格納個数
+	int							n_net_sim;			//格納信号線数
 
 } FFR;
 
