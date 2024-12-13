0edf59c09b7281b05fe5b2656db43373d052a881
tok0901 <pingyerenxi@gmail.com>
2024-12-13 19:38:07 +0900

    Update input_f.c

--- input_f.c
+++ input_f.c
@@ -2,19 +2,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 #include "Netlist.h"
 #include "Queue.h"
 #include "Command.h"
 #include "Fault_dic.h"
 #include "FFR.h"
+#include "HASH.h"
 
 int input_f(char* testfile, char* pinfile, char* vfile) {
 
 	FILE* fp_test, * fp_pin;
 	fp_test = fopen(testfile, "r");
 	fp_pin = fopen(pinfile, "r");
-	int i,sim_test = 64;
+	int i, sim_test = 64;
 
 	if (fp_test == NULL) {
 		printf("テスト集合ファイルが読み込めません\n");
@@ -59,7 +61,7 @@ int input_f(char* testfile, char* pinfile, char* vfile) {
 	char* buf_test;
 	buf_test = (char*)malloc(sizeof(char) * n_pi);
 
-	while (fgets(buf_test, n_pi*2, fp_test) != NULL) {
+	while (fgets(buf_test, n_pi * 2, fp_test) != NULL) {
 		if (strcmp(buf_test, "\n") != 0) {	//改行のみの記述をカウントしないように制御
 			n_test++;
 		}
@@ -73,7 +75,7 @@ int input_f(char* testfile, char* pinfile, char* vfile) {
 
 	//テストパターンファイル読み込み
 	i = 0;
-	while (fgets(buf_test, n_pi*2, fp_test) != NULL) {
+	while (fgets(buf_test, n_pi * 2, fp_test) != NULL) {
 
 		if (strcmp(buf_test, "\n") != 0) {
 			strcpy(Str_test[i], buf_test);
@@ -90,11 +92,8 @@ int input_f(char* testfile, char* pinfile, char* vfile) {
 	//信号線の論理値配列(64bit)の領域確保
 	//信号線のDetectability配列(64bit)の領域確保→long long intとして修正,ビット演算として処理
 	for (i = 0; i < n_net; i++) {
-		nl[i].value = (short*)malloc(sizeof(short) * sim_test);
-		nl[i].detec = (short*)malloc(sizeof(short) * sim_test);
-		nl[i].value_fault_flag = (short*)malloc(sizeof(short) * sim_test);
-		nl[i].score[0] = 0.0;
-		nl[i].score[1] = 0.0;
+		nl[i].val = 0ULL;
+		nl[i].fval = 0ULL;
 		nl[i].score_flag = 0;
 		nl[i].total_score = 0.0;
 		nl[i].tpi_flag = 0;
@@ -110,15 +109,12 @@ int input_f(char* testfile, char* pinfile, char* vfile) {
 		dic[test_number].tp = test_number;
 		dic[test_number].n_grp = 0;
 		dic[test_number].insert_number = 0;
-		//dic[test_number].unconf_fault = (NLIST***)malloc(sizeof(NLIST**));	//1つの格納配列のみ宣言
-		//dic[test_number].po_value = (char**)malloc(sizeof(char*));			//1つのハッシュ表のみ宣言
-		//dic[test_number].unconf_saf_flag = (int**)malloc(sizeof(int*));		//1つの検出故障フラグ配列のみ宣言
-		//dic[test_number].n_unconf_fault = (int*)malloc(sizeof(int));		//1つの未識別故障数のみ宣言
+		
 	}
 
 	//未識別故障集合格納配列の領域確保,初期化
 	hash.unconf_fault = (NLIST***)malloc(sizeof(NLIST**) * n_net * 2);
-	hash.saf_flag = (short**)malloc(sizeof(short*) * n_net * 2);
+	hash.unconf_saf_fault = (ULLI**)malloc(sizeof(ULLI*) * n_net * 2);
 	hash.n_unconf_fault = (int*)malloc(sizeof(int) * n_net * 2);
 	hash.n_index = (int*)malloc(sizeof(int) * n_net * 2);
 	hash.n_grp = 0;
@@ -128,14 +124,19 @@ int input_f(char* testfile, char* pinfile, char* vfile) {
 	hash.n_confirm_flag = (short*)malloc(sizeof(short) * n_net * 2);
 
 	//観測ポイント配列の領域確保,観測ポイント数初期化
-	tpi_net = (NLIST**)malloc(sizeof(NLIST*) * (n_net - n_po));
+	n_tpi_po = n_po;
+	tpi_po_net = (NLIST**)malloc(sizeof(NLIST*) * n_tpi_po);
 	n_tpi = 0;
+
+	for (int po_number = 0; po_number < n_po; po_number++) {
+		tpi_po_net[po_number] = po[po_number];
+	}
 
 	fclose(fp_test);
 
 	fclose(fp_pin);
 
-	printf("ファイル入力完了!\n\n\n");
+	//printf("\n\n\nファイル入力完了!\n\n\n");
 
 	return 1;
 
